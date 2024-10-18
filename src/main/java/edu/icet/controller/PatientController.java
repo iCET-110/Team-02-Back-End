bcf9f526e9e7dd6ff1af655ba8035e3b25ea1868
jlakvidu <jlakvidu@gmail.com>
2024-10-18 16:18:55 +0530

    add patient controller

--- src/main/java/edu/icet/controller/PatientController.java
+++ src/main/java/edu/icet/controller/PatientController.java
@@ -1,4 +1,24 @@
 package edu.icet.controller;
 
+import edu.icet.dto.Patient;
+import edu.icet.service.PatientService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/patient")
+@RequiredArgsConstructor
+@Slf4j
 public class PatientController {
+    private final PatientService patientService;
+
+    @PostMapping("/add-patient")
+    public void addPatient(@RequestBody Patient patient){
+        log.info("Received Patient: {}", patient);
+        patientService.addStudent(patient);
+    }
 }
