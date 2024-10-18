dee83820c96bde42a2fa5a0e3a06db5911cff1fd
jlakvidu <jlakvidu@gmail.com>
2024-10-18 16:23:08 +0530

    add patient controller

--- src/main/java/edu/icet/repository/PatientDao.java
+++ src/main/java/edu/icet/repository/PatientDao.java
@@ -3,5 +3,5 @@ package edu.icet.repository;
 import edu.icet.dto.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PatientDao {
+public interface PatientDao extends JpaRepository<Patient,String> {
 }
