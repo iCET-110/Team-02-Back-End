bab138d7785fb723937d8eecbc49383eb3478955
githubmage2002 <pasinduchithakaa17@gmail.com>
2024-10-18 15:52:14 +0530

    delete guardian done

--- src/main/java/edu/icet/controller/GuardiantController.java
+++ src/main/java/edu/icet/controller/GuardiantController.java
@@ -1,19 +1,15 @@
 package edu.icet.controller;
-
 import edu.icet.service.GuardiantService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-
 @RestController
 @CrossOrigin
 @RequestMapping("/Guardian")
 @RequiredArgsConstructor
 @Slf4j
 public class GuardiantController {
-
     final GuardiantService guardiantService;
-
     @DeleteMapping("/delete-guardian/{id}")
     public void deleteGuardiant(@PathVariable String id){
         guardiantService.deleteGuardiant(id);
--- src/main/java/edu/icet/service/impl/GuardiantServiceImpl.java
+++ src/main/java/edu/icet/service/impl/GuardiantServiceImpl.java
@@ -1,16 +1,12 @@
 package edu.icet.service.impl;
-
 import edu.icet.repository.GuardiantDao;
 import edu.icet.service.GuardiantService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 @Service
 @RequiredArgsConstructor
 public class GuardiantServiceImpl implements GuardiantService {
-
     final GuardiantDao guardiantDao;
-
     @Override
     public void deleteGuardiant(String id) {
         guardiantDao.deleteById(id);
