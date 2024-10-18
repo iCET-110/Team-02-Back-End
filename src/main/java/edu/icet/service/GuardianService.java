815daf125c7505257157650da8aba0e4319b7b2d
Lakru99 <lakru999@gmail.com>
2024-10-18 15:38:51 +0530

    add guardian

---
+++ src/main/java/edu/icet/service/GuardianService.java
@@ -0,0 +1,11 @@
+package edu.icet.service;
+
+import edu.icet.dto.Guardian;
+
+import java.util.List;
+
+public interface GuardianService {
+    List<Guardian> getGuardian();
+    void addGuardian(Guardian guardian);
+
+}
