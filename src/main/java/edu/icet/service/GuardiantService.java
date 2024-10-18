81c682ec2f2d635f9c773925a5bdeee573d5ae0c
githubmage2002 <pasinduchithakaa17@gmail.com>
2024-10-18 16:24:52 +0530

    delete guardiant done

--- src/main/java/edu/icet/service/GuardiantService.java
+++ src/main/java/edu/icet/service/GuardiantService.java
@@ -1,4 +1,4 @@
 package edu.icet.service;
 public interface GuardiantService {
-
+    void deleteGuardiant(String id);
 }
--- src/main/java/edu/icet/service/impl/GuardiantServiceImpl.java
+++ src/main/java/edu/icet/service/impl/GuardiantServiceImpl.java
@@ -3,8 +3,12 @@ import edu.icet.repository.GuardiantDao;
 import edu.icet.service.GuardiantService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-@Service
 @RequiredArgsConstructor
-public class GuardiantServiceImpl  {
-
+@Service
+public class GuardiantServiceImpl implements GuardiantService{
+    final GuardiantDao guardiantDao;
+    @Override
+    public void deleteGuardiant(String id) {
+        guardiantDao.deleteById(id);
+    }
 }
