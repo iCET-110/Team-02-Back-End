3e4716d1ec840c62b495124ec9fe60b951125051
Eternatus9999 <eternatus9999@gmail.com>
2024-10-21 12:41:43 +0530

    fix a file issue

--- src/main/java/edu/icet/repository/GuardiantDao.java
+++
@@ -1,7 +0,0 @@
-package edu.icet.repository;
-
-import edu.icet.dto.Guardian;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GuardiantDao extends JpaRepository<Guardian,String> {
-}
--- src/main/java/edu/icet/service/GuardiantService.java
+++
@@ -1,8 +0,0 @@
-package edu.icet.service;
-
-import edu.icet.dto.Guardian;
-
-public interface GuardiantService {
-    public void updateGuardian(Guardian guardiant);
-
-}
