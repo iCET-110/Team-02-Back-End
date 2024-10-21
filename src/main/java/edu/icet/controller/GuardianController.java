9039eb32104ae315ba617f799a5d71077434eef3
githubmage2002 <pasinduchithakaa17@gmail.com>
2024-10-21 14:01:09 +0530

    commiting to get a pull from guardian-controller branch

--- src/main/java/edu/icet/controller/GuardianController.java
+++ src/main/java/edu/icet/controller/GuardianController.java
@@ -1,5 +1,27 @@
 package edu.icet.controller;
 
-public class GuardianController {
+import edu.icet.service.GuardianService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
+@RequestMapping("/Guardian")
+@RestController
+@Slf4j
+public class GuardianController {
+    final GuardianService guardianService;
+
+    @DeleteMapping("/delete/all")
+    public void deleteGuardianAll(HttpServletRequest request){
+        String ip = request.getRemoteAddr();
+        log.info("Request Received Delete-All {}",ip);
+
+    }
+    @DeleteMapping("/delete/{id}")
+    public void deleteGuardianById(@PathVariable String id ,HttpServletRequest request){
+        String ip = request.getRemoteAddr();
+        log.info("Request Received Delete-{} {}",id,ip);
+    }
 }
