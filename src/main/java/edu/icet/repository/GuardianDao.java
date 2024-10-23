f28f34614df550cac00150ab11825023042c656b
Kalhara302 <kalhara.dev@gmail.com>
2024-10-23 12:40:14 +0530

    update GuardianDao

--- src/main/java/edu/icet/repository/GuardianDao.java
+++ src/main/java/edu/icet/repository/GuardianDao.java
@@ -3,7 +3,12 @@ package edu.icet.repository;
 import edu.icet.entity.GuardianEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface GuardianDao extends JpaRepository<GuardianEntity,String> {
+    List<GuardianEntity> findByGuardianName(String name);
+
+    GuardianEntity findByGuardianContact(String contact);
 
 }
