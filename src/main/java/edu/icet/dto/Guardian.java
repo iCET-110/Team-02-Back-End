3e40cfde86efa4cde3a7f204d43045cda981eb0f
Thanuja-99 <thanujakumarasekara@gmail.com>
2024-10-23 11:29:53 +0530

    Guardian Model Class Validation

--- src/main/java/edu/icet/dto/Guardian.java
+++ src/main/java/edu/icet/dto/Guardian.java
@@ -1,4 +1,6 @@
 package edu.icet.dto;
+
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +12,14 @@ import lombok.ToString;
 @ToString
 public class Guardian {
 
+    @NotBlank(message = "Guardian name is required")
     private String guardianName;
+
+    @NotBlank(message = "Guardian NIC is required")
+    @Pattern(regexp = "^[0-9]{9}[Vv]$", message = "Guardian NIC should be 9 digits followed by 'V' or 'v'")
     private String guardianNIC;
+
+    @NotBlank(message = "Guardian contact is required")
+    @Pattern(regexp = "^[0-9]{10}$", message = "Guardian contact should be a 10-digit number")
     private String guardianContact;
 }
